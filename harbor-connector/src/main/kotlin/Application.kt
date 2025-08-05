8c6bd7f47f1b888b612841f5282cd17daa8d21a1
kleonova <kleonova0@gmail.com>
2025-08-05 17:55:28 +0800

    add harbor-connector service

--- harbor-connector/src/main/kotlin/Application.kt
+++ harbor-connector/src/main/kotlin/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import lev.learn.sandbox.harbor.connector.route.harborConnectorRoutes
@@ -12,10 +13,14 @@ fun main() {
     val config = ApplicationConfig("application.conf")
     val port = config.property("ktor.deployment.port").getString().toInt()
 
+    val serviceName = config.property("name").getString()
+
     embeddedServer(Netty, port = port) {
+        install(CallLogging)
+
         routing {
             get("/") {
-                call.respondText("Harbor Connector Service is running!")
+                call.respondText("$serviceName is running on $port!")
             }
 
             harborConnectorRoutes()
